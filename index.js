e1cb368f2e72a6aa4f09d0dae83e081ddc56e9f7
sashikn <sashi.kanth@traveloka.com>
2020-03-12 13:41:05 +0530

    convert structure of rules

--- index.js
+++ index.js
@@ -6,20 +6,71 @@ let dynamoDBClient = require("./dynamoDBClient");
 let getAllRules = async () => {
     try {
         let params = {
-            TableName : config.DYNAMO_TABLE_NAME
+            TableName: config.DYNAMO_TABLE_NAME
         }
 
         let data = await dynamoDBClient.scan(params).promise();
+        console.log("Get Rules data : ", JSON.stringify(data));
 
-
-
-        console.log("data", data);
+        return data;
     } catch (error) {
-        console.log(error);
+        console.log("Get Rules data : ", JSON.stringify(error));
+        return {
+            Items: [],
+            Count: 0,
+            ScannedCount: 0
+        }
+    }
+}
+
+
+convertDataToNewStructure = (ruleList) => {
+    try {
+
+        for (let i = 0; i < ruleList.length; i++) {
+            const rule = ruleList[i];
+            rule.recommendationToolRuleList = rule.recommendationToolRuleList
+                .sort((a, b) => {
+                    return a.index > b.index ? 1 : -1;
+                });
+
+
+            rule.operands = [];
+
+            for (let j = 0; j < rule.recommendationToolRuleList.length; j++) {
+                const ru = rule.recommendationToolRuleList[j];
+                let obj = {};
+                obj.depth = 0;
+                obj.operands = [];
+                obj.deleted = ru.deleted;
+                obj.index = ru.index;
+                obj.joinOperator = ru.joinOperator;
+                obj.rule = { ...ru };
+                rule.operands.push(obj);
+            }
+
+            rule.recommendationToolRuleList = null;
+        }
+
+        console.log("Convert Data List : ", JSON.stringify(ruleList));
+
+        return ruleList;
+    } catch (error) {
+        console.log("Convert data : ", error);
+        return [];
     }
 }
 
 
 
 
-getAllRules();
+let init = async () => {
+    let data = await getAllRules();
+
+    let ruleList = convertDataToNewStructure(data.Items);
+
+}
+
+
+
+init();
