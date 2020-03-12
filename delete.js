0340ba3bb8d3efc184ff9d9241d467ae34d4a3a0
sashikn <sashi.kanth@traveloka.com>
2020-03-12 14:12:21 +0530

    delete one-by-one

--- delete.js
+++ delete.js
@@ -3,18 +3,49 @@ let dynamoDBClient = require("./dynamoDBClient");
 
 
 
-let deleteData = async () =>{
+
+
+
+let deleteData = async (ruleList) => {
+    try {
+        
+        for (let i = 0; i < ruleList.length; i++) {
+            const rule = ruleList[i];
+            let params = {
+                TableName: config.DYNAMO_NESTED_TABLE_NAME,
+                Key: {
+                    id : rule.id
+                }
+            }
+
+            let data = await dynamoDBClient.delete(params).promise();
+
+
+            console.log("Delete an Item : ", data);
+        }
+    } catch (error) {
+        console.log("Delete error : ", error);
+    }
+}
+
+
+let getAllRules = async () => {
     try {
         let params = {
-            TableName : config.DYNAMO_NESTED_TABLE_NAME,
+            TableName: config.DYNAMO_TABLE_NAME
         }
 
+        let data = await dynamoDBClient.scan(params).promise();
+        console.log("Get Rules data : ", (data));
 
-        let data = await dynamoDBClient.delete(params).promise();
-
-        console.log("Delete data : ", data);
+        return data;
     } catch (error) {
-        console.log("Delete data error : ", error);
+        console.log("Get Rules data : ", JSON.stringify(error));
+        return {
+            Items: [],
+            Count: 0,
+            ScannedCount: 0
+        }
     }
 }
 
@@ -23,9 +54,10 @@ let deleteData = async () =>{
 
 
 
-
 let init = async () => {
-    await deleteData();
+
+    let data = await getAllRules();d
+    await deleteData(data.Items);
 
 }
 
